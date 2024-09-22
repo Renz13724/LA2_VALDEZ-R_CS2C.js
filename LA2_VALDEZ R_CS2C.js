292fbe064cc62a755ecf3ffc91bfb25a5ae94561
VALDEZ R. <renzv753@gmail.com>
2024-09-22 13:48:21 +0800

    Create LA2_VALDEZ R_CS2C.js

---
+++ LA2_VALDEZ R_CS2C.js
@@ -0,0 +1,19 @@
+let firstFavNum = 13;
+let secondFavNum = 24;
+
+let sumOfall = firstFavNum + secondFavNum;
+let f1Addf2 = firstFavNum + secondFavNum;
+let f1Subtractf2 = firstFavNum -  secondFavNum;
+let f1MultiplyF2 = firstFavNum * secondFavNum;
+let f1Dividedf2 =firstFavNum / secondFavNum;
+let f1Modf2 = firstFavNum % secondFavNum;
+let f1ToThePowerf2 = firstFavNum ** secondFavNum;
+
+
+console.log(sumOfall);
+console.log(f1Addf2);
+console.log(f1Subtractf2);
+console.log(f1MultiplyF2);
+console.log(f1Dividedf2);
+console.log(f1Modf2);
+console.log(f1ToThePowerf2);
